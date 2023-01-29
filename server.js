4745d26d63162715d4e5ce1245330a12807ae50d
Raghav Mehta <Raghav@MacBook-Pro.local>
2023-01-29 16:49:53 +0530

    live-cursor-fixed

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const {Server} = require('socket.io');
 const cors = require('cors');
 
-// const grahak = require('./grahak');
 
 const app = express();
 app.use(cors());
@@ -12,34 +11,64 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
    cors: {
-    origin: "http://localhost:3000",
+    origin: "http://192.168.106.88:3000",
     methods: ["GET","POST"],
    }
 });
+
+let cursorPositions = [];
 
 io.on("connection", (socket) => {
     console.log(`A client has connected... ${socket.id}`);
 
     socket.on("join_room", (room) => {
+        // Change in room check 
+        // Updating cursors and presence
         if ((socket.rooms.size) > 1){
             let oldRoom = Array.from(socket.rooms)[1];
             console.log(oldRoom);
             socket.leave(oldRoom);
             updateUsersInRoom(oldRoom);
+            cursorPositions.forEach(function(obj,index) {
+                if (obj.socketId === socket.id) {
+                    cursorPositions.splice(index,1);
+                }
+            });
         };
+        // Joining new room
         socket.join(room);
-        updateUsersInRoom(room);
+        let newElement = {socketId : socket.id, room : room, x : 0, y : 0};
+        cursorPositions.push(newElement);
+        // console.log(cursorPositions);
         console.log(`user ${socket.id} has joined ${room}`);
+        updateUsersInRoom(room);
+        cursorUpdate(room,socket.id);
     });
-
+    // Listening to mouse movement and updating cursor positions
     socket.on("mouseMove", (data) => {
-        socket.to(Array.from(socket.rooms)[1]).emit("mouseMoved", data);
+        cursorPositions.forEach(function(obj){
+            if(obj.socketId === socket.id){
+                obj.x = data.x;
+                obj.y = data.y;
+            }
+        });
+        // Emitting cursors back to the room
+        let room = Array.from(socket.rooms)[1];
+        cursorUpdate(room,socket.id);
     })
 
+    // Disconnecting room
     socket.on("disconnecting", () => {
         console.log(`user ${socket.id} disconnected`);
         let room = Array.from(socket.rooms)[1];
+        let id = socket.id
+        cursorPositions.forEach(function(obj,index) {
+            if (obj.socketId === id) {
+                cursorPositions.splice(index,1);
+            }
+        });
         socket.leave(room);
+        cursorUpdate(room,id);
         updateUsersInRoom(room);
     }); 
 });
@@ -47,6 +76,14 @@ io.on("connection", (socket) => {
 async function updateUsersInRoom(room) {
     io.to(room).emit("newUserCount", (await io.in(room).fetchSockets()).length);
 };
- 
+
+function cursorUpdate(room, id){
+    let roomCursors = cursorPositions
+        .filter(obj => obj.room === room)
+        .map( obj => ({socketId: obj.socketId, x: obj.x, y: obj.y}));
+    io.to(room).emit("cursorUpdate", roomCursors);
+    // console.log(cursorPositions);
+    console.log(roomCursors);
+};
 
 server.listen(3001, ()=> {console.log('Server chal raha hain BC!')});
--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import './App.css';
 import io from 'socket.io-client';
 import { useEffect, useState } from 'react';
 
-const cursorUrlArray = [
-  'https://icons.iconarchive.com/icons/svengraph/daft-punk/256/Daft-Punk-Guyman-Off-icon.png', 
-  'https://icons.iconarchive.com/icons/everaldo/starwars/128/Darth-Vader-icon.png', 
-  'https://icons.iconarchive.com/icons/everaldo/starwars/128/clone-old-icon.png',
-  'https://icons.iconarchive.com/icons/svengraph/daft-punk/256/Daft-Punk-Thomas-On-icon.png'
-];
+// const cursorUrlArray = [
+//   'https://icons.iconarchive.com/icons/svengraph/daft-punk/256/Daft-Punk-Guyman-Off-icon.png', 
+//   'https://icons.iconarchive.com/icons/everaldo/starwars/128/Darth-Vader-icon.png', 
+//   'https://icons.iconarchive.com/icons/everaldo/starwars/128/clone-old-icon.png',
+//   'https://icons.iconarchive.com/icons/svengraph/daft-punk/256/Daft-Punk-Thomas-On-icon.png'
+// ];
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("http://192.168.106.88:3001");
 
 function App() {
   const [userCount, setUserCount] = useState("");
-  const[otherCursor, setOtherUser] = useState({x:0, y:0});
+  const[otherCursors, setOtherUsers] = useState([]);
 
-  const [cursorUrl, setCursorUrl] = useState("");
+  // const [cursorUrl, setCursorUrl] = useState("");
   
   useEffect(() => {
     socket.on("newUserCount", (count) => {
@@ -26,28 +26,21 @@ function App() {
       socket.emit("mouseMove", {x: event.clientX, y: event.clientY});
     }
 
-    // Add event listener for mouse movement {part of DOM elemen}
+    // Add event listener for mouse movement {part of DOM element}
     document.addEventListener("mousemove", handleMouseMove);
 
     // Handle broadcasted mouseMoved event from server
-    socket.on("mouseMoved", (data) => {
-      setOtherUser({x: data.x, y: data.y});
+    socket.on("cursorUpdate", (data) => {
+      setOtherUsers(data);
     });
     
-    setCursorUrl(cursorUrlArray[Math.floor(Math.random() * cursorUrlArray.length)]);
+    // setCursorUrl(cursorUrlArray[Math.floor(Math.random() * cursorUrlArray.length)]);
 
-    // Listen to removeCursor event and remove cursor element from DOM
-    // socket.on("removeCursor", (socketId) => {
-    //   const cursor = document.getElementById(`cursor-${socketId}`);
-    //   if (cursor) {
-    //       cursor.remove();
-    //   }
-    // });
 
     // Remove event listener when component unmounts
-    return () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-    };
+    // return () => {
+    //   document.removeEventListener("mousemove", handleMouseMove);
+    // };
 
   },[]);
 
@@ -66,14 +59,21 @@ function App() {
       }}>Join Room 2</button>
     </p>
     There are {userCount} users in the room.
-    
-      <div
-        className='other-cursor'
-        style={{
-          left: otherCursor.x,
-          top: otherCursor.y,
-          backgroundImage: `url(${cursorUrl})`
-        }} />
+    {otherCursors.map((cursor) => {
+      if(cursor.socketId !== socket.id){
+      return(
+        <div 
+          key={cursor.socketId} 
+          className="other-cursor" 
+          style={{
+            left: cursor.x,
+            top: cursor.y,
+            // transition: "transform 120ms linear",
+            // transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`,
+          }}
+        />);
+      } else {return null;}
+    })}
     </div>
   );
 }
